2e602009bef9d4996c351b0c6ef8f26d6cb99e83
mjcjiang <redmagic039@gmail.com>
2023-08-16 17:17:12 +0800

    code commit

--- apps/test_cap_parser.cc
+++ apps/test_cap_parser.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "cap_parser.h"
 
 int main(int argc, char *argv[])
 {
+    CapParser parser("./files/nd_packet.cap");
+    parser.processPackages();
     return 0;
 }
 
