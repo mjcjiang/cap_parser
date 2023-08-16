01ba39200f4d22438070d67d573522feede95cfa
hjiang <hjiang@hjiang>
2023-08-16 04:27:17 -0700

    commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Prerequisite
+g++ >= std11
+cmake >= 3.12
+
+# Compile and Link
+mkdir build
+cd build
+cmake ..
+make
+
+# Run
+cd apps
+./test_cap_parser
--- src/cap_parser.cc
+++ src/cap_parser.cc
@@ -5,8 +5,14 @@
 #include <netinet/udp.h>
 #include <spdlog/spdlog.h>
 
-#define MAC_ADDR_LEN 20
-#define ETHER_HEADER_LEN sizeof(ether_header)
+constexpr auto MAC_ADDR_LEN = 20;
+constexpr auto ETHER_HEADER_LEN = sizeof(ether_header);
+
+void decode_mac(char *mac_str, const u_char *src_mac) {
+    sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            src_mac[0], src_mac[1], src_mac[2],
+            src_mac[3], src_mac[4], src_mac[5]);
+}
 
 CapParser::CapParser(const std::string& filename) {
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -22,6 +28,7 @@ void CapParser::processPackages() {
     while((packet = pcap_next(pCapHandler_.get(), &header)) != NULL) {
         processHeaders(packet);
     }
+    SPDLOG_INFO("Finish parse .cap file");
 }
 
 void CapParser::processHeaders(const u_char* packet) {
@@ -29,44 +36,45 @@ void CapParser::processHeaders(const u_char* packet) {
     struct ether_header* eth_header = (struct ether_header*)packet;
     const u_char* src_mac = eth_header->ether_shost;
     const u_char* dst_mac = eth_header->ether_dhost;
-
     char src_mac_str[MAC_ADDR_LEN];
     char dst_mac_str[MAC_ADDR_LEN];
-    sprintf(src_mac_str, "%02x:%02x:%02x:%02x:%02x:%02x",
-            src_mac[0], src_mac[1], src_mac[2],
-            src_mac[3], src_mac[4], src_mac[5]);
-    sprintf(dst_mac_str, "%02x:%02x:%02x:%02x:%02x:%02x",
-            dst_mac[0], dst_mac[1], dst_mac[2],
-            dst_mac[3], dst_mac[4], dst_mac[5]);
+    decode_mac(src_mac_str, src_mac);
+    decode_mac(dst_mac_str, dst_mac);
 
     //协议类型解析
     struct ip* ip_header = (struct ip*)(packet + ETHER_HEADER_LEN);
     int protocol = ip_header->ip_p;
 
     if (protocol == IPPROTO_TCP) {
-        struct tcphdr* tcp_header = (struct tcphdr*)(packet + ETHER_HEADER_LEN + ip_header->ip_hl * 4); // Skip IP header
+        struct tcphdr* tcp_header = (struct tcphdr*)(packet +
+                                                     ETHER_HEADER_LEN +
+                                                     ip_header->ip_hl * 4); // Skip IP header
+
         uint16_t src_port = ntohs(tcp_header->th_sport);
         uint16_t dst_port = ntohs(tcp_header->th_dport);
         uint32_t seq_num = ntohl(tcp_header->th_seq);
 
-        std::cout << "Tcp Protocol: " << std::endl;
+        std::cout << "Protocol: " << "TCP" << std::endl;
         std::cout << "Source Address: " << inet_ntoa(ip_header->ip_src) << std::endl;
         std::cout << "Source Port: " << src_port << std::endl;
         std::cout << "Dest Address: " << inet_ntoa(ip_header->ip_dst) << std::endl;
         std::cout << "Dest Port: " << dst_port << std::endl;
         std::cout << "Sequence Number: " << seq_num << std::endl;
     } else if(protocol == IPPROTO_UDP) {
-        struct udphdr* udp_header = (struct udphdr*)(packet + ETHER_HEADER_LEN + ip_header->ip_hl * 4); // Skip IP header
+        struct udphdr* udp_header = (struct udphdr*)(packet +
+                                                     ETHER_HEADER_LEN +
+                                                     ip_header->ip_hl * 4); // Skip IP header
+
         uint16_t src_port = ntohs(udp_header->uh_sport);
         uint16_t dst_port = ntohs(udp_header->uh_dport);
 
-        std::cout << "Udp Protocol" << std::endl;
+        std::cout << "Protocol: " << "UDP" << std::endl;
         std::cout << "Source Address: " << inet_ntoa(ip_header->ip_src) << std::endl;
         std::cout << "Source Port: " << src_port << std::endl;
         std::cout << "Dest Address: " << inet_ntoa(ip_header->ip_dst) << std::endl;
         std::cout << "Dest Port: " << dst_port << std::endl;
     }
+
     std::cout << "Source MAC: " << src_mac_str << std::endl;
     std::cout << "Destination MAC: " << dst_mac_str << std::endl;
 }
-
